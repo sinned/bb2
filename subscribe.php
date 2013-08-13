fb99c78943a3e23bff70a746bcecdc98a1c7aa7a
Dennis Yang <sinned@gmail.com>
2013-08-13 00:39:11 -0700

    fixing recipient name

--- subscribe.php
+++ subscribe.php
@@ -120,7 +120,7 @@
 						</ul>
 						<div id='whofor' class='<?php echo $subfor=='gift' ? '' : 'hide'; ?>'>
 							<h5>What is the name of the recipient?</h5>
-							<input type="text" value='Giftee'/>
+							<input type="text" value='Lucky Recipient&rsquo;s Name' placeholder='Lucky Recipient&rsquo;s Name' />
 						</div>
 					</div>
 				</div>
