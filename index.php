894d1d06ef0ca4374dc9b9db8f2d37dffa3d3d62
Dennis Yang <sinned@gmail.com>
2013-09-06 16:41:51 -0700

    changing instagram widget to 4 now.

--- index.php
+++ index.php
@@ -37,7 +37,8 @@
 	<div class="row">
 		<div class="large-12 column hide-for-small text-center">
 			<h6>The bar you need for the drinks you love</h6>
-			<iframe src="http://snapwidget.com/in/?u=Yml0dGVyc2FuZGJvdHRsZXN8aW58MjAwfDN8MXx8eWVzfDV8bm9uZXxvblN0YXJ0" allowTransparency="true" frameborder="0" scrolling="no" style="border:none; overflow:hidden; width:645px; height: 215px" ></iframe>
+<!-- SnapWidget -->
+<iframe src="http://snapwidget.com/in/?u=Yml0dGVyc2FuZGJvdHRsZXN8aW58MjIwfDR8MXx8bm98NXxub25lfG9uU3RhcnR8eWVz&v=6913" allowTransparency="true" frameborder="0" scrolling="no" style="border:none; overflow:hidden; width:900px; height: 225px" ></iframe>
 		</div>
 	</div>
 	
