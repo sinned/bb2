812dc17be4be3e29fb09457a33e75106fecff384
Dennis Yang <sinned@gmail.com>
2013-09-13 01:54:22 -0700

    shop stuff.

--- shop/index.php
+++ shop/index.php
@@ -17,31 +17,31 @@
 		</div>
 	</div>
 	<div class="row">
-		<div class="large-3 column">
-			<h4>Bulleit Bourbon</h4>
-			<a class="th radius" href="<?php echo WEBROOT; ?>img/shop/bulleit-bourbon.jpg"><img src="<?php echo WEBROOT; ?>img/shop/bulleit-bourbon.jpg" alt="Bulleit Bourbon" /></a>
-			<br /><br />
-			<form action="https://bittersandbottles.foxycart.com/cart" method="post">
-				<input type="hidden" name="name" value="Bulleit Bourbon" />
-				<input type="hidden" name="price" value="49.00" />
-				<inpyt type="hidden" name="image" value="<?php echo WEBROOT; ?>img/shop/bulleit-bourbon.jpg" />
-				<input type="submit" value="Add to Cart" />
-			</form>		
-		</div>
-		<div class="large-3 column">
-			<h4>Bulleit Bourbon</h4>
-			<a class="th radius" href="<?php echo WEBROOT; ?>img/shop/bulleit-bourbon.jpg"><img src="<?php echo WEBROOT; ?>img/shop/bulleit-bourbon.jpg" alt="Bulleit Bourbon" /></a>
-			<br /><br />
-			<form action="https://bittersandbottles.foxycart.com/cart" method="post">
-				<input type="hidden" name="name" value="Bulleit Bourbon" />
-				<inpyt type="hidden" name="image" value="<?php echo WEBROOT; ?>img/shop/bulleit-bourbon.jpg" />				
-				<input type="hidden" name="price" value="49.00" />
-				<input type="submit" value="Add to Cart" />
-			</form>					
-		</div>
-		<div class="large-3 column">
-		</div>
-		<div class="large-3 column">
+		<div class="large-12 column">
+			<table class="shop">
+				<tr>
+					<td>
+						<h4>Bulleit Bourbon</h4>
+						<img src="<?php echo WEBROOT; ?>img/shop/bulleit-bourbon.jpg" alt="Bulleit Bourbon" />
+						<form action="https://bittersandbottles.foxycart.com/cart" method="post">
+							<input type="hidden" name="name" value="Bulleit Bourbon" />
+							<input type="hidden" name="price" value="25.00" />
+							<inpyt type="hidden" name="image" value="<?php echo WEBROOT; ?>img/shop/bulleit-bourbon.jpg" />
+							<input type="submit" value="Add to Cart" />
+						</form>		
+					</td>
+					<td>
+						<h4>Diet Coke</h4>
+						<img src="<?php echo WEBROOT; ?>img/shop/diet-coke.jpg" alt="Diet Coke" />
+						<form action="https://bittersandbottles.foxycart.com/cart" method="post">
+							<input type="hidden" name="name" value="Diet Coke" />
+							<inpyt type="hidden" name="image" value="<?php echo WEBROOT; ?>img/shop/diet-coke.jpg" />				
+							<input type="hidden" name="price" value="1.00" />
+							<input type="submit" value="Add to Cart" />
+						</form>				
+					</td>
+				</tr>
+			</table>
 		</div>
 	</div>
 	<?php include "../includes/footer.php"; ?>
