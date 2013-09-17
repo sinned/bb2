2ba199b89cbd5da62992988363a986612cee2241
Dennis Yang <sinned@gmail.com>
2013-09-16 18:06:48 -0700

    adding a space to the title.

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <head>
 	<meta charset="utf-8">
   <meta name="viewport" content="width=device-width">
-  <title>Bitters+Bottles</title>
+  <title>Bitters + Bottles</title>
   <?php include "includes/scripts.php"; ?>
 
 </head>
