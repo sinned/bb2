cc6b812a2cda22784560c00c7df15cae5e6d6b87
Dennis Yang <sinned@gmail.com>
2013-10-29 10:40:38 -0700

    re-adding the california&ny line

--- index.php
+++ index.php
@@ -14,11 +14,13 @@
 	<div class="row">
 		<div id="marquee" class="large-12 column">
 			<img src="img/build-the-perfect-home-bar.jpg" title="B + B delivers Classic cocktail ingredients and recipes, and rare spirits monthly." alt="B + B delivers Classic cocktail ingredients and recipes, and rare spirits monthly" />
-			<p class="right"><small>Sign-up for monthly deliveries tailored to your style: master mixer or armchair sipper.</p>
+			<p class="right"><small>Now serving California <span class="bbplus">+</span> New York.</small></p>
 		</div>
 	</div>
 	<div class="row">
-		&nbsp;
+		<div class="large-12 column text-center">
+			<h4>Sign-up for monthly deliveries tailored to your style: master mixer or armchair sipper.</h4>
+		</div>
 	</div>
 	<div class="row">
 		<div class="large-6 small-6 column card">
