59a482878b8fafeff9c9dccec5e2216ed9c757e9
Dennis Yang <sinned@gmail.com>
2014-09-15 18:47:27 -0700

    october edit

--- subscribe.php
+++ subscribe.php
@@ -275,7 +275,7 @@
 					</div>
 					<div class="large-10 column price_description">
 						<p>
-							September 15th<?php //echo date('F', strtotime($subscription_starttimestring)); ?>
+							October 15th<?php //echo date('F', strtotime($subscription_starttimestring)); ?>
 						</p>
 					</div>
 				</div>				
